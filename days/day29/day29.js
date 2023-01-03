de9a182e313ef4df6095cf52012c51579ea91efc
Simon <sema92m@wp.pl>
2023-01-03 20:41:52 +0100

    Day 29 exercise complete

--- days/day29/day29.js
+++ days/day29/day29.js
@@ -1,26 +1,44 @@
 const body = document.body;
-const text = document.querySelector("p");
-const fonts = ['Arial', 'Verdana', 'Tahoma', 'Times New Roman', 'Georgia', 'Brush Script MT'];
-// let str = "30 DAYS OF JAVASCRIPT CHALLENGE 2020 SZYMON BRYCZKOWSKI";
+body.style.margin = '15px';
+body.style.fontSize = '8vh';
+body.style.fontWeight = '900';
+
+let str = "30 DAYS OF JAVASCRIPT CHALLENGE 2020 SZYMON BRYCZKOWSKI";
+const text = document.createElement("div");
+text.setAttribute('class', 'text');
+text.style.animation = 'opacity 2s infinite';
+text.style.height = '90vh';
+
+const fonts = [
+    "Arial",
+    "Verdana",
+    "Tahoma",
+    "Times New Roman",
+    "Georgia",
+    "Brush Script MT",
+];
+
+body.insertAdjacentElement('afterbegin', text);
 
 
-
-body.insertAdjacentElement("afterbegin", text);
-
-
-
-
-
-
-
-
-
-
+function colorizeEachLetter() {
+    let arrFromStr = str.split('');
+    let newFont = Math.floor(Math.random() * fonts.length);
+    let HTMLtext = ``;
+    for(let i = 0; i < arrFromStr.length; i++){ 
+        HTMLtext += `<span style='color:${hexaColor()}'>${arrFromStr[i]}</span>`;
+    }
+    
+    text.style.fontFamily = fonts[newFont];
+    text.style.background =  `linear-gradient(to right, ${hexaColor()} 0%, ${hexaColor()} 20%, ${hexaColor()} 61%), linear-gradient(${hexaColor()} 52%, ${hexaColor()} 60%, ${hexaColor()} 61%)`;
+    text.innerHTML = HTMLtext;
+}
+setInterval(colorizeEachLetter,2000);
 
 
 function hexaColor() {
     const str = "0123456789abcdef";
-    let hexa = "#";
+    let hexa = '#';
     let index;
     for (let i = 0; i < 6; i++) {
         index = Math.floor(Math.random() * str.length);
@@ -28,3 +46,9 @@ function hexaColor() {
     }
     return hexa;
 }
+
+
+
+
+
+
