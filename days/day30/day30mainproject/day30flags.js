026fe2e32f841e22d388138944b3f28bcfd17cf0
Simon <sema92m@wp.pl>
2023-01-13 22:40:20 +0100

    Main project 45% complete

--- days/day30/day30mainproject/day30flags.js
+++ days/day30/day30mainproject/day30flags.js
@@ -1,218 +1,71 @@
 let countryName = "";
 let countryCapital = "";
 let countryPopulation = "";
-let countryLanguages = [];
-let lang = "";
+let languages = "";
 let countryFlagURL = "";
-let cardArr = [];
+let elem = [];
+let arr = [];
 
 const wrapper = document.querySelector(".wrapper");
-
-
-
 
 
 const url = "https://restcountries.com/v3.1/all";
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        data.forEach((element) => {
-            countryCapital = element.capital;
-            if(!countryCapital) {
-                countryCapital = '';
-            }
-            lang = element.languages;
-            countryName = element.name.common;
-            countryPopulation = element.population;
-            countryFlagURL = element.flags.svg;
-            const card = document.createElement("div");
-            card.setAttribute('class', 'card');
-            card.innerHTML = `  <img style='background-image: url(${countryFlagURL})' class="flag"></img>
-                                <div class="country-name">${element.name.common}</div>
-                                <div class="country-info">
-                                    <div class="capital">${countryCapital[0]}</div>
-                                    <div class="languages"></div>
-                                    <div class="population">${countryPopulation}</div>
-                                </div>
-                            `;
-                            wrapper.appendChild(card);
-            // return console.log(countryName,countryCapital[0], countryPopulation,countryFlagURL);
-        });
+        // arr = [...data];
+        arr.push(data);
+        // console.log(arr);
+        // return arr;
+        // data.forEach((element) => {
+        //     countryCapital = element.capital;
+        // if(countryCapital == undefined || !countryCapital) {
+        //     countryCapital = 'None capital';
+        // }
+        // countryName = element.name.common;
+        // countryPopulation = element.population;
+        // countryFlagURL = element.flags.svg;
+        // elem.push(countryCapital,countryName,countryPopulation,countryFlagURL);
+        // });
+        // arr.push(elem);
     })
     .catch((error) => console.error(error));
-// cardArr.push(countryName, countryPopulation, countryFlagURL);
 
 
-let elem = {
-    name: {
-        common: "Grenada",
-        official: "Grenada",
-        nativeName: {
-            eng: {
-                official: "Grenada",
-                common: "Grenada",
-            },
-        },
-    },
-    tld: [".gd"],
-    cca2: "GD",
-    ccn3: "308",
-    cca3: "GRD",
-    cioc: "GRN",
-    independent: true,
-    status: "officially-assigned",
-    unMember: true,
-    currencies: {
-        XCD: {
-            name: "Eastern Caribbean dollar",
-            symbol: "$",
-        },
-    },
-    idd: {
-        root: "+1",
-        suffixes: ["473"],
-    },
-    capital: ["St. George's"],
-    altSpellings: ["GD"],
-    region: "Americas",
-    subregion: "Caribbean",
-    languages: {
-        eng: "English",
-    },
-    translations: {
-        ara: {
-            official: "غرينادا",
-            common: "غرينادا",
-        },
-        bre: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        ces: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        cym: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        deu: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        est: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        fin: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        fra: {
-            official: "Grenade",
-            common: "Grenade",
-        },
-        hrv: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        hun: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        ita: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        jpn: {
-            official: "グレナダ",
-            common: "グレナダ",
-        },
-        kor: {
-            official: "그레나다",
-            common: "그레나다",
-        },
-        nld: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        per: {
-            official: "گرنادا",
-            common: "گرنادا",
-        },
-        pol: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        por: {
-            official: "Grenada",
-            common: "Granada",
-        },
-        rus: {
-            official: "Гренада",
-            common: "Гренада",
-        },
-        slk: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        spa: {
-            official: "Granada",
-            common: "Grenada",
-        },
-        swe: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        tur: {
-            official: "Grenada",
-            common: "Grenada",
-        },
-        urd: {
-            official: "گریناڈا",
-            common: "گریناڈا",
-        },
-        zho: {
-            official: "格林纳达",
-            common: "格林纳达",
-        },
-    },
-    latlng: [12.11666666, -61.66666666],
-    landlocked: false,
-    area: 344,
-    demonyms: {
-        eng: {
-            f: "Grenadian",
-            m: "Grenadian",
-        },
-        fra: {
-            f: "Grenadienne",
-            m: "Grenadien",
-        },
-    },
-    flag: "🇬🇩",
-    maps: {
-        googleMaps: "https://goo.gl/maps/rqWyfUAt4xhvk1Zy9",
-        openStreetMaps: "https://www.openstreetmap.org/relation/550727",
-    },
-    population: 112519,
-    fifa: "GRN",
-    car: {
-        signs: ["WG"],
-        side: "left",
-    },
-    timezones: ["UTC-04:00"],
-    continents: ["North America"],
-    flags: {
-        png: "https://flagcdn.com/w320/gd.png",
-        svg: "https://flagcdn.com/gd.svg",
-    },
-    coatOfArms: {
-        png: "https://mainfacts.com/media/images/coats_of_arms/gd.png",
-        svg: "https://mainfacts.com/media/images/coats_of_arms/gd.svg",
-    },
-    startOfWeek: "monday",
-    capitalInfo: {
-        latlng: [32.38, -64.68],
-    },
-};
+
+function main() {
+    console.log(arr);
+    // console.log(arr.flat());
+    arr.forEach((element) => {
+        console.log(element);
+        countryCapital = element.capital;
+        if(countryCapital == undefined || !countryCapital) {
+            countryCapital = 'None capital';
+        }
+        //1)take arr values(langs) from language obj, 2) remove more then 8 languages from arr
+        //3) make string from array of languages (without spaces), 4) add spaces after comma;
+        if(element.languages) {
+            languages = ((Object.values(element.languages)).slice(0,8).toString()).replace(/,/g, ', ');
+        }
+        //
+        countryName = element.name.common;
+        countryPopulation = element.population;
+        countryFlagURL = element.flags.svg;
+        const card = document.createElement("div");
+        card.setAttribute('class', 'card');
+        card.innerHTML = 
+        `  
+            <img style='background-image: url(${countryFlagURL})' class="flag"></img>
+            <div class="country-name">${element.name.common}</div>
+            <div class="country-info">
+                <div class="capital">Capital: ${countryCapital}</div>
+                <div class="languages">Languages: ${languages}</div>
+                <div class="population">Population: ${countryPopulation}</div>
+            </div>
+        `;
+                        wrapper.appendChild(card);
+    });
+    
+}
+
+main();
