cd93788e3c4b89e7d9665100a0d48cdef0fdaf5c
Simon <sema92m@wp.pl>
2023-01-15 20:53:22 +0100

    Main funciton(fetch) complete, Add pre-load spinner

--- days/day30/day30mainproject/day30flags.js
+++ days/day30/day30mainproject/day30flags.js
@@ -1,105 +1,79 @@
-let countryName = "";
-let countryCapital = "";
-let countryPopulation = "";
-let languages = "";
-let countryFlagURL = "";
-let elem = [];
-let countries = [];
-let countries1 = [];
-let countriesFinal = [];
-
-const wrapper = document.querySelector(".wrapper");
-const mainInput = document.querySelector("#mainInput");
-const countriesCountH5 = document.querySelector(".countries-count");
-
 const url = "https://restcountries.com/v3.1/all";
+let countries = [];
+const wrapper = document.querySelector(".wrapper");
+const searchInput = document.querySelector("#mainInput");
+const subheader = document.querySelector(".subheader");
+const countriesCountH5 = document.querySelector(".countries-count");
+const spinner = document.querySelector(".spinner");
+
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data.sort((a,b) => b.name.common - a.name.common));
         for (let i = 0; i < data.length; i++) {
             if (data[i].capital === undefined || !data[i].capital) {
                 data[i].capital = ['None capital'];
             }
-            countries.push(data[i].name.common,(data[i].capital).toString(),data[i].population);
-            // console.log(data[i].name.common, data[i].population, data[i].flags.svg);
+            if(!data[i].languages || data[i].languages == undefined){
+                data[i].languages = '';
+            }
+            let langs = (Object.values(data[i].languages).slice(0,5)).toString().replace(/,/g, ', ');
+            countries.push([data[i].name.common,(data[i].capital[0]).toString(),data[i].population,data[i].flags.png, langs]);
         }
-        console.log(countries);
-        // countries = [...data];
-        // countries.forEach((element) => {
-        //     countryName = element.name.common;
-        //     countryPopulation = element.population;
-        //     countryFlagURL = element.flags.svg;
-        //     countryCapital = element.capital;
-        //     if(countryCapital == undefined || !countryCapital) {
-        //         countryCapital = 'None capital';
-        //     }
-        //     if(element.languages) {
-        //         languages = ((Object.values(element.languages)).slice(0,8).toString()).replace(/,/g, ', ');
-        //     }
-
-        //     const card = document.createElement("div");
-        //     card.setAttribute('class', 'card');
-        //     card.innerHTML = 
-        //     `  
-        //         <img style='background-image: url(${countryFlagURL})' class="flag"></img>
-        //         <div class="country-name">${element.name.common}</div>
-        //         <div class="country-info">
-        //             <div class="capital">Capital: ${countryCapital}</div>
-        //             <div class="languages">Languages: ${languages}</div>
-        //             <div class="population">Population: ${countryPopulation}</div>
-        //         </div>
-        //     `;
-        //     wrapper.appendChild(card);
-            
-
-        // });
-        // countries1.push(countryName,countryPopulation,countryFlagURL,countryCapital,languages);
-        // console.log(countries1);
-        // countriesFinal.push(countries1);
-        // // console.log(countriesFinal);
-        // countriesCountH5.textContent = `${wrapper.childElementCount} countries sitisfield the search criteria`;
-        // return countries;
+        reloadCards(countries);
+        subheader.textContent = `Currently, we have ${countries.length} countries`;
+        countriesCountH5.textContent = `${wrapper.children.length - 1} countries sitisfield the search criteria`;
+        spinnerDisplayBlock();
+        
     })
     .catch((error) => console.error(error));
 
 
-mainInput.addEventListener('input', searchFunction);
-
-function searchFunction() {
-    if(mainInput.value == '') {
-
-    }
-    wrapper.innerHTML = '';
+function reloadCards(arr) {
+    arr.forEach((element) => {
+        const card = document.createElement("div");
+        card.setAttribute('class', 'card');
+        card.innerHTML = 
+            `  
+                <img style='background-image: url(${element[3]})' class="flag"></img>
+                <div class="country-name">${element[0]}</div>
+                <div class="country-info">
+                    <div class="capital">Capital: ${element[1]}</div>
+                    <div class="languages">Languages: ${element[4]}</div>
+                    <div class="population">Population: ${element[2].toLocaleString("en-US")}</div>
+                </div>
+            `;
+        wrapper.appendChild(card);
+    });
 }
 
 
-// function main() {
-//     countries.forEach((element) => {
-//         countryName = element.name.common;
-//         countryPopulation = element.population;
-//         countryFlagURL = element.flags.svg;
-//         countryCapital = element.capital;
-//         if(countryCapital == undefined || !countryCapital) {
-//             countryCapital = 'None capital';
-//         }
-//         //1)take arr values(langs) from language obj, 2) remove more then 8 languages from arr
-//         //3) make string from array of languages (without spaces), 4) add spaces after comma;
-//         if(element.languages) {
-//             languages = ((Object.values(element.languages)).slice(0,8).toString()).replace(/,/g, ', ');
-//         }
-//         const card = document.createElement("div");
-//         card.setAttribute('class', 'card');
-//         card.innerHTML = 
-//         `  
-//             <img style='background-image: url(${countryFlagURL})' class="flag"></img>
-//             <div class="country-name">${element.name.common}</div>
-//             <div class="country-info">
-//                 <div class="capital">Capital: ${countryCapital}</div>
-//                 <div class="languages">Languages: ${languages}</div>
-//                 <div class="population">Population: ${countryPopulation}</div>
-//             </div>
-//         `;
-//         wrapper.appendChild(card);
-//     });
+
+
+
+// searchInput.addEventListener('input', (event) => {
+    // const searchValue = event.target.value.toLowerCase();
+    // if (searchValue == "") {
+    //     wrapper.innerHTML = '';
+    //     reloadCards(countries);
+    // }
+    //  
+
+// });
+
+// function searchFunction() {
+//     if(searchInput.value == '') {
+            // reloadCards(countries);
+//     }
+//     wrapper.innerHTML = '';
 // }
+
+
+
+
+
+
+function spinnerDisplayBlock() {
+    if(wrapper.children.length > 1) {
+        spinner.style.display = 'none';
+    } 
+}
