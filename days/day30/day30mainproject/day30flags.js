4b23e2ab85f4be1003bcc353670eecceb5b8f133
Simon <sema92m@wp.pl>
2023-01-16 22:52:03 +0100

    Add sort by population button and sort function

--- days/day30/day30mainproject/day30flags.js
+++ days/day30/day30mainproject/day30flags.js
@@ -1,10 +1,17 @@
 const url = "https://restcountries.com/v3.1/all";
 let countries = [];
+let searchCountryAndCapitalFromInput = [];
 const wrapper = document.querySelector(".wrapper");
 const searchInput = document.querySelector("#mainInput");
 const subheader = document.querySelector(".subheader");
 const countriesCountH5 = document.querySelector(".countries-count");
 const spinner = document.querySelector(".spinner");
+
+const nameButton = document.querySelector("#name-button");
+const capitalButton = document.querySelector("#capital-button");
+const populationButton = document.querySelector("#population-button");
+const sortButton = document.querySelector("#sort-button");
+
 
 fetch(url)
     .then((response) => response.json())
@@ -16,13 +23,13 @@ fetch(url)
             if(!data[i].languages || data[i].languages == undefined){
                 data[i].languages = '';
             }
-            let langs = (Object.values(data[i].languages).slice(0,5)).toString().replace(/,/g, ', ');
+            let langs = (Object.values(data[i].languages).slice(0,8)).toString().replace(/,/g, ', ');
             countries.push([data[i].name.common,(data[i].capital[0]).toString(),data[i].population,data[i].flags.png, langs]);
         }
         reloadCards(countries);
         subheader.textContent = `Currently, we have ${countries.length} countries`;
-        countriesCountH5.textContent = `${wrapper.children.length - 1} countries sitisfield the search criteria`;
-        spinnerDisplayBlock();
+        countriesCountH5.textContent = `${wrapper.children.length} countries satisfied the search criteria`;
+        spinnerDisplayNone();
         
     })
     .catch((error) => console.error(error));
@@ -38,11 +45,12 @@ function reloadCards(arr) {
                 <div class="country-name">${element[0]}</div>
                 <div class="country-info">
                     <div class="capital">Capital: ${element[1]}</div>
-                    <div class="languages">Languages: ${element[4]}</div>
                     <div class="population">Population: ${element[2].toLocaleString("en-US")}</div>
+                    <div class="languages">Languages: ${element[4]}</div>
                 </div>
             `;
         wrapper.appendChild(card);
+        countriesCountH5.textContent = `${wrapper.children.length} countries satisfied the search criteria`;
     });
 }
 
@@ -50,29 +58,93 @@ function reloadCards(arr) {
 
 
 
-// searchInput.addEventListener('input', (event) => {
-    // const searchValue = event.target.value.toLowerCase();
-    // if (searchValue == "") {
-    //     wrapper.innerHTML = '';
-    //     reloadCards(countries);
-    // }
-    //  
+searchInput.addEventListener('input', (event) => {
+    const searchValue = event.target.value.toLowerCase().trim();
+    if (searchValue === "") {
+        wrapper.innerHTML = '';
+        searchCountryAndCapitalFromInput = countries;
+        reloadCards(countries);
+    }
+    if (searchValue !== "")  {
+        wrapper.innerHTML = '';
+        createCardFromInputSearch();
+    } 
+});
 
-// });
 
-// function searchFunction() {
-//     if(searchInput.value == '') {
-            // reloadCards(countries);
-//     }
-//     wrapper.innerHTML = '';
+function createCardFromInputSearch() {
+    // searchCountryAndCapitalFromInput.length = 0;
+    for(let country of countries) {
+        let countryName = country[0].toLowerCase();
+        let countryCapital = country[1].toLowerCase();
+        if (countryName.startsWith(`${searchInput.value}`) || 
+        countryCapital.startsWith(`${searchInput.value}`) || 
+        countryName.includes(`${searchInput.value}`) ||
+        countryCapital.includes(`${searchInput.value}`)) {
+            searchCountryAndCapitalFromInput.push(country);
+        }
+    }
+    
+    reloadCards(searchCountryAndCapitalFromInput);
+}
+
+// function sortByName() {
+//     searchCountryAndCapitalFromInput.sort( (a, b) => a.localeCompare(b) );
+// }
+// function sortByCapital() {
+//     searchCountryAndCapitalFromInput.sort((a,b) => b[1] - a[1]);
+
+//     console.log( searchCountryAndCapitalFromInput.sort((a,b) => b[1] - a[1]));
 // }
 
+function sortByPopulation() {
+    populationButton.classList.toggle('active1');
+    populationButton.textContent = 'POPULATION ↑';
+    if (wrapper.children.length === 250 && populationButton.classList.contains('active1')) {
+        wrapper.innerHTML = '';
+        reloadCards(countries.sort((a,b) => b[2] - a[2]));
+        populationButton.textContent = 'POPULATION ↑';
+    }
+    if (wrapper.children.length === 250 && !populationButton.classList.contains('active1')) {
+        wrapper.innerHTML = '';
+        reloadCards(countries.sort((a,b) => a[2] - b[2]));
+        populationButton.textContent = 'POPULATION ↓';
+    }
+    // if ((searchCountryAndCapitalFromInput.length === 0 || !searchCountryAndCapitalFromInput) && populationButton.classList.contains('active1')) {
+    //     wrapper.innerHTML = '';
+    //     reloadCards(countries.sort((a,b) => b[2] - a[2]));
+    //     populationButton.textContent = 'POPULATION ↑';
+    // } if ((searchCountryAndCapitalFromInput.length === 0 || !searchCountryAndCapitalFromInput) && !populationButton.classList.contains('active1')) {
+    //     wrapper.innerHTML = '';
+    //     reloadCards(countries.sort((a,b) => a[2] - b[2]));
+    //     populationButton.textContent = 'POPULATION ↓';
+    // }
+    if ((searchCountryAndCapitalFromInput.length > 0) && populationButton.classList.contains('active1')) {
+        wrapper.innerHTML = '';
+        reloadCards(searchCountryAndCapitalFromInput.sort((a,b) => b[2] - a[2]));
+        populationButton.textContent = 'POPULATION ↑';
+    } 
+    if ((searchCountryAndCapitalFromInput.length > 0) && !populationButton.classList.contains('active1')) {
+        wrapper.innerHTML = '';
+        reloadCards(searchCountryAndCapitalFromInput.sort((a,b) => a[2] - b[2]));
+        populationButton.textContent = 'POPULATION ↓';
+    }
+}  
+
+// nameButton.addEventListener('click', sortByName);
+// capitalButton.addEventListener('click', sortByCapital);
+populationButton.addEventListener('click', sortByPopulation);
 
 
 
 
 
-function spinnerDisplayBlock() {
+
+
+
+
+
+function spinnerDisplayNone() {
     if(wrapper.children.length > 1) {
         spinner.style.display = 'none';
     } 
